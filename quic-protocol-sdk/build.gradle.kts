d64d7fabd3c4844f38dd4fedbcf8c2dbf0dabd8c
nero <gaosilaifu.he@gmail.com>
2021-06-30 18:40:02 +0800

    update gradle script

--- quic-protocol-sdk/build.gradle.kts
+++ quic-protocol-sdk/build.gradle.kts
@@ -18,60 +18,46 @@ kotlin {
 
     // Create target for the host platform.
     val hostTarget = when {
-        hostOs == "Mac OS X" -> macosX64("quic-protocol-sdk")
-        hostOs == "Linux" -> linuxX64("quic-protocol-sdk")
-        hostOs.startsWith("Windows") -> mingwX64("quic-protocol-sdk")
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        hostOs.startsWith("Windows") -> mingwX64("native")
         else -> throw GradleException("Host OS '$hostOs' is not supported in Kotlin/Native $project.")
     }
 
     hostTarget.apply {
-
-
-        compilations["main"].enableEndorsedLibs = true
-    }
-
-    // Enable experimental stdlib API used by the sample.
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-stdlib")
-            }
-        }
-
-        linuxX64("native").apply {
-            binaries {
-                sharedLib {
+        binaries {
+            sharedLib {
 //                entryPoint = "nero.quic-protocol-sdk.main"
-                    baseName = "msquic"
+                baseName = "msquic"
+                when (preset) {
+                    presets["macosX64"] -> linkerOpts(
+                        "-L${rootProject.projectDir}/quic-protocol-sdk/msquic/bin",
+                        "-Lmsquic"
+                    )
+                    presets["linuxX64"] -> linkerOpts(
+                        "-L${rootProject.projectDir}/quic-protocol-sdk/msquic/bin",
+                        "-Lmsquic"
+                    )
+                    presets["mingwX64"] -> linkerOpts("-L${mingwPath.resolve("${rootProject.projectDir}/quic-protocol-sdk/msquic/bin")}")
+                }
+            }
+
+
+            compilations["main"].cinterops {
+                val msquic by creating {
                     when (preset) {
-                        presets["macosX64"] -> linkerOpts(
-                            "-L${rootProject.projectDir}/quic-protocol-sdk/msquic/bin",
-                            "-Lmsquic"
+                        presets["macosX64"] -> includeDirs.headerFilterOnly(
+                            "/${rootProject.projectDir}/quic-protocol-sdk/msquic/",
+                            "/usr/local/include"
                         )
-                        presets["linuxX64"] -> linkerOpts(
-                            "-L${rootProject.projectDir}/quic-protocol-sdk/msquic/bin",
-                            "-Lmsquic"
+                        presets["linuxX64"] -> includeDirs.headerFilterOnly(
+                            "/${rootProject.projectDir}/quic-protocol-sdk/msquic/",
+                            "/usr/include"
                         )
-                        presets["mingwX64"] -> linkerOpts("-L${mingwPath.resolve("${rootProject.projectDir}/quic-protocol-sdk/msquic/bin")}")
+                        presets["mingwX64"] -> includeDirs(mingwPath.resolve("${rootProject.projectDir}/quic-protocol-sdk/msquic/"))
                     }
                 }
-
-
-                compilations["main"].cinterops {
-                    val msquic by creating {
-                        when (preset) {
-                            presets["macosX64"] -> includeDirs.headerFilterOnly(
-                                "/${rootProject.projectDir}/quic-protocol-sdk/msquic/",
-                                "/usr/local/include"
-                            )
-                            presets["linuxX64"] -> includeDirs.headerFilterOnly(
-                                "/${rootProject.projectDir}/quic-protocol-sdk/msquic/",
-                                "/usr/include"
-                            )
-                            presets["mingwX64"] -> includeDirs(mingwPath.resolve("${rootProject.projectDir}/quic-protocol-sdk/msquic/"))
-                        }
-                    }
-                }
+            }
 //            val sdl by creating {
 //                when (preset) {
 //                    presets["macosX64"] -> includeDirs("/opt/local/include/SDL2", "/usr/local/include/SDL2")
@@ -79,13 +65,27 @@ kotlin {
 //                    presets["mingwX64"] -> includeDirs(mingwPath.resolve("include/SDL2"))
 //                }
 //            }
-            }
-            dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-stdlib")
-            }
         }
 
+        compilations["main"].enableEndorsedLibs = true
     }
+
+    // Enable experimental stdlib API used by the sample.
+//    sourceSets {
+//        commonMain {
+//            dependencies {
+//                implementation("org.jetbrains.kotlin:kotlin-stdlib")
+//            }
+//        }
+//
+////        linuxX64("native").apply {
+////
+////            dependencies {
+////                implementation("org.jetbrains.kotlin:kotlin-stdlib")
+////            }
+////        }
+//
+//    }
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
